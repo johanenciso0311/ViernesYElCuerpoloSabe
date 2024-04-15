7b6410af617f569926fdb473b3496a93b3b0e0ab
steven2.0 <encisoholguinsteven@gmial.com>
2024-04-15 16:20:56 +0200

    pepe

---
+++ src/main/java/EjercicioCajaNegra/Ejercicio1/Excursion.java
@@ -0,0 +1,27 @@
+package EjercicioCajaNegra.Ejercicio1;
+
+public class Excursion {
+
+    private  int numeroAlumnos;
+
+    public int getNumeroAlumnos() {
+        return numeroAlumnos;
+    }
+
+    public void setNumeroAlumnos(int numeroAlumnos) {
+        this.numeroAlumnos = numeroAlumnos;
+    }
+
+    public Excursion(int numeroAlumnos) {
+        this.numeroAlumnos = numeroAlumnos;
+    }
+
+    public double CalcularPrecioAlumno(){
+        return 0;
+    }
+
+    public  double CalcularCosteExcursion() {
+        return 0;
+    }
+
+}
--- src/main/java/EjercicioCajaNegra/Ejercicio1/ExcursionMetodosEstaticos.java
+++ src/main/java/EjercicioCajaNegra/Ejercicio1/ExcursionMetodosEstaticos.java
@@ -1,4 +1,13 @@
 package EjercicioCajaNegra.Ejercicio1;
 
 public class ExcursionMetodosEstaticos {
+
+    public static double CalcularPrecioAlumno(int numeroAlumnos){
+        return 0;
+    }
+
+    public static double CalcularCosteExcursion(){
+        return 0;
+    }
+
 }
